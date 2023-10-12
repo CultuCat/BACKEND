a8382b1a48f70e0bb36b81d7485ce7b44ec8f869
Hector Emanuel Peña Cuevas <emanuel.cuevas03@gmail.com>
2023-10-12 12:51:40 +0200

    error ssl, datos db ya puestos

--- cultucat/settings.py
+++ cultucat/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'cultucat.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': ‘<db_name>’,
-        'USER': '<db_username>',
-        'PASSWORD': '<password>',
-        'HOST': '<db_hostname_or_ip>',
+        'NAME': 'cultucatbd',
+        'USER': 'admin',
+        'PASSWORD': 'pes04',
+        'HOST': '147.83.148.217',
         'PORT': '40390',
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
 
