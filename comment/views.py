5c4f59d3d45e1555630c76ea63a924188aa65164
Marc Román <120638990+marcrq@users.noreply.github.com>
2023-10-25 11:17:51 +0200

    Revert "#65 post comment"

--- comment/views.py
+++ comment/views.py
@@ -4,31 +4,21 @@ from .models import Comment
 from .serializers import CommentSerializer
 from user.permissions import IsAuthenticated 
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.pagination import PageNumberPagination
 
 class CommentsView(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    models = Comment
-    pagination_class = PageNumberPagination
-    pagination_class.page_size = 10
-    #permission_classes = True#[IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['event']
-    
-    apply_permissions = True
-
-    def get_permissions(self):
-        if self.action == 'create' and self.apply_permissions:
-            return [IsAuthenticated]
-        else:
-            return []
+    filterset_fields = {
+        'event__id': ['exact', 'in']
+    }
 
     def create(self, request, *args, **kwargs):
-        comment = request.data.copy()
-        comment['user'] = 1 #request.user #1
-        serializer = self.get_serializer(data=comment)
+        data = request.data.copy()
+        data['user'] = request.user.id #1
+        serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
