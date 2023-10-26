4e20a2f2ac2f8ef763fbdc5158f5c75af4c31331
Marc Román <marc.roman.quilez@estudiantat.upc.edu>
2023-10-26 21:28:28 +0200

    fix?

--- comment/serializers.py
+++ comment/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user.username", read_only=True)
     class Meta:
         model = Comment
-        fields = ['id', 'user', 'event', 'text', 'created_at']
+        fields = ['id', 'text', 'created_at', 'event', 'username']
