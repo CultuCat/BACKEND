216328c43992c428a87a9ffd75199abb25107f45
christianrm02 <christianrm02@gmail.com>
2023-11-16 20:58:29 +0100

    add: changed returned values by demand front

--- ticket/views.py
+++ ticket/views.py
@@ -1,13 +1,12 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Ticket
-from .serializers import TicketSerializer
+from .serializers import TicketSerializer, TicketSerializer_byEvent, TicketSerializer_byUser
 from user.permissions import IsAuthenticated 
 from django_filters.rest_framework import DjangoFilterBackend
 
 class TicketsView(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
-    serializer_class = TicketSerializer
     models = Ticket
     #permission_classes = [IsAuthenticated]
 
@@ -21,6 +20,18 @@ class TicketsView(viewsets.ModelViewSet):
             return [IsAuthenticated]
         else:
             return []
+        
+    def get_serializer_class(self):
+        user_param = self.request.query_params.get('user', None)
+        event_param = self.request.query_params.get('event', None)
+
+        # Determinar el serializador basado en los parámetros de la solicitud
+        if user_param is not None and event_param is None:
+            return TicketSerializer_byUser
+        elif event_param is not None and user_param is None:
+            return TicketSerializer_byEvent
+        else:
+            return TicketSerializer
 
     def create(self, request, *args, **kwargs):
         ticket = request.data.copy()
