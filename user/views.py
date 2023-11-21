16c7eb63070cd4ba623651fe98f8b87a6874ede1
{nuriacanals3} <{nuria.canals4@gmail.com}>
2023-11-21 21:22:29 +0100

    block user by admin

--- user/views.py
+++ user/views.py
@@ -16,6 +16,7 @@ from tags.models import Tag
 from spaces.models import Space
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from user.permissions import IsAdmin
 
 
 class PerfilView(viewsets.ModelViewSet):
@@ -123,6 +124,23 @@ class PerfilView(viewsets.ModelViewSet):
             return Response({'detail': 'La resta dels usuaris et poden trobar'}, status=status.HTTP_200_OK)
         else:
             return Response({'detail': 'La resta dels usuaris no et poden trobar'}, status=status.HTTP_200_OK)
+        
+    @action(detail=True, methods=['PUT'])
+    def block_profile(self, request, pk=None):
+        perfil = self.get_object()
+        is_blocked = request.data.get('isBlocked')
+
+        if request.user.is_staff:
+            
+            perfil.isBlocked = is_blocked
+            perfil.save()
+
+            if perfil.isBlocked == True:
+                return Response({'detail': "L'usuari està bloquejat"}, status=status.HTTP_200_OK)
+            else:
+                return Response({'detail': "L'usuari no està bloquejat"}, status=status.HTTP_200_OK)
+        else:
+            return Response({'detail': 'No tens permisos per fer aquesta acció'}, status=status.HTTP_403_FORBIDDEN)
 
     
 @api_view(['POST'])
