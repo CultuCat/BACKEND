d9b5dfa5fe4519947a8fe97e2d5e4a685f3c59d4
christianrm02 <christianrm02@gmail.com>
2023-11-21 17:05:39 +0100

    change: other urls for qrs

--- ticket/models.py
+++ ticket/models.py
@@ -5,9 +5,9 @@ import random
 
 def get_random_image():
     IMAGE_CHOICES = [
-        'https://static.vecteezy.com/system/resources/previews/002/258/271/large_2x/template-of-qr-code-ready-to-scan-with-smartphone-illustration-vector.jpg',
+        'https://shre.ink/Tmac',
         'https://th.bing.com/th/id/OIP.T0ywp7K4Ke4LifNoo6n1zwHaFS?rs=1&pid=ImgDetMain',
-        'https://www.elegantthemes.com/blog/wp-content/uploads/2019/05/How-to-create-a-QR-code-Elegant-Themes-Blog.png',
+        'https://shre.ink/Tmaj',
     ]
     return random.choice(IMAGE_CHOICES)
 
