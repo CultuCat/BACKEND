59e12388a64a99cd45d4616bc01cccee74a77bc6
christianrm02 <christianrm02@gmail.com>
2023-11-27 20:30:40 +0100

    add: new attribute actual level achived trophy

--- trophy/serializers.py
+++ trophy/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from .models import Trophy
+from discount.models import Discount
 
 class TrophySerializer(serializers.ModelSerializer):
+    level_achived_user = serializers.SerializerMethodField()
+    
     class Meta:
         model = Trophy
-        fields = '__all__'
+        fields = ['nom', 'descripcio', 'punts_nivell1', 'punts_nivell2', 'punts_nivell3', 'level_achived_user']
+        
+    def get_level_achived_user(self, obj):
+        user_id = self.context['request'].user.id
+        ds = Discount.objects.filter(userDiscount=user_id, nomTrofeu=obj.nom)
+        max = -1
+        for d in ds:
+            if d.nivellTrofeu > max:
+                max = d.nivellTrofeu
+        return max
