6c7d9be5671cf8884935b04f810681a15ea6cc73
Marc Román <marc.roman.quilez@estudiantat.upc.edu>
2023-12-12 14:57:40 +0100

    add: new endpoint this_week and pagination

--- events/urls.py
+++ events/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     *router.urls,
     re_path('home', EventView.as_view({'get': 'home'}), name='home events'),
     re_path('today', EventView.as_view({'get': 'today'}), name='today events'),
+    re_path('this_week', EventView.as_view({'get': 'this_week'}), name='this week events'),
 ]
