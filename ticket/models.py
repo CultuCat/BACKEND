94303e9f689052fbd6058bad63d254f6e01fdaaa
Marc Román <marc.roman.quilez@estudiantat.upc.edu>
2023-12-21 16:22:45 +0100

    fix: refactor qrs

--- ticket/models.py
+++ ticket/models.py
@@ -5,17 +5,17 @@ import random
 
 def get_random_image():
     IMAGE_CHOICES = [
-        'https://storage.cloud.google.com/cultucat_bucket/images/EncontradoQR.png',
-        'https://storage.cloud.google.com/cultucat_bucket/images/PatacasQR.png',
-        'https://storage.cloud.google.com/cultucat_bucket/images/GolasoQR.png',
-        'https://storage.cloud.google.com/cultucat_bucket/images/NanoQR.png',
+        'images/EncontradoQR.png',
+        'images/PatacasQR.png',
+        'images/GolasoQR.png',
+        'images/NanoQR.png',
     ]
     return random.choice(IMAGE_CHOICES)
 
 class Ticket(models.Model):
     user = models.ForeignKey(Perfil, on_delete=models.CASCADE, null=False, blank=False, related_name='tickets')
     event = models.ForeignKey(Event, on_delete=models.CASCADE, null=False, blank=False)
-    image = models.CharField(default=get_random_image, max_length=200)
+    image = models.ImageField(default=get_random_image)
 
     class Meta:
         unique_together = ('user', 'event')
