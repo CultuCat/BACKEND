0728ca88c9b33865d3a7713a3c6527b0bb213e83
Marc Román <marc.roman.quilez@estudiantat.upc.edu>
2023-12-21 18:33:59 +0100

    fix: images

--- ticket/models.py
+++ ticket/models.py
@@ -13,21 +13,14 @@ IMAGE_CHOICES = [
 def get_random_image():
     return random.choice(IMAGE_CHOICES)
 
-
 class Ticket(models.Model):
     user = models.ForeignKey(Perfil, on_delete=models.CASCADE, null=False, blank=False, related_name='tickets')
     event = models.ForeignKey(Event, on_delete=models.CASCADE, null=False, blank=False)
-    image = models.CharField(max_length=255, blank=True) #IMAGE_CHOICES[random.randint(0, len(IMAGE_CHOICES)-1)]
-    
-    def save(self, *args, **kwargs):
-        if not self.image:
-            self.image = get_random_image()
-        super().save(*args, **kwargs)
+    image = models.ImageField(default=get_random_image)
 
     class Meta:
         unique_together = ('user', 'event')
         ordering = ['event__dataIni']
-        
         
     def __str__(self):
         return f"Ticket by {self.user} on {self.event}"
