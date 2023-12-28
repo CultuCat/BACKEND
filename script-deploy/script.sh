6db8fa8722cbd331a37f3e87b4c0e32cc1863fb5
Hector Emanuel Peña Cuevas <emanuel.cuevas03@gmail.com>
2023-12-28 08:42:06 +0100

    Update script.sh

--- script-deploy/script.sh
+++ script-deploy/script.sh
@@ -1,4 +1,4 @@
-gcloud compute ssh cultucat-back --zone=us-central1-a --ssh-key-file ${{ secrets.GCP_SSH_PRIVATE_KEY }} --tunnel-through-iap --project=cultucat-405114
+gcloud compute ssh cultucat-back --zone=us-central1-a --ssh-key-file "$GCP_SSH_PRIVATE_KEY" --tunnel-through-iap --project=cultucat-405114
 sudo -i -u emanuel.cuevas03
 cd backend/
 source myenv/bin/activate
