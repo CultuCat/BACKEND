a8c1c913a676b24cacae413ae52e2648d5542ce9
Hector Emanuel Peña Cuevas <emanuel.cuevas03@gmail.com>
2023-12-28 09:29:51 +0100

    final fixes

--- script-deploy/script.sh
+++ script-deploy/script.sh
@@ -5,16 +5,9 @@ fi
 gcloud compute config-ssh --quiet
 
 gcloud compute ssh cultucat-back --zone=us-central1-a --tunnel-through-iap --project=cultucat-405114 << EOF
-
-pwd
-whoami
 sudo -i -u emanuel.cuevas03
-pwd
-whoami
-echo "soy yo"
 cd backend/
 source myenv/bin/activate
 git pull
 sudo service apache2 restart
-
 EOF
