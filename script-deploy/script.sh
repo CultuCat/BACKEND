0a3dba73d8753a0a6707bbc076f01c2e23693fe9
Hector Emanuel Peña Cuevas <emanuel.cuevas03@gmail.com>
2023-12-28 08:49:31 +0100

    Update script.sh

--- script-deploy/script.sh
+++ script-deploy/script.sh
@@ -1,6 +1,13 @@
-gcloud compute ssh cultucat-back --zone=us-central1-a --ssh-key-file "$GCP_SSH_PRIVATE_KEY" --tunnel-through-iap --project=cultucat-405114
-sudo -i -u emanuel.cuevas03
-cd backend/
-source myenv/bin/activate
-git pull
-sudo service apache2 restart
+if [ ! -f ~/.ssh/google_compute_engine ]; then
+  ssh-keygen -t rsa -f ~/.ssh/google_compute_engine -N ''
+fi
+
+gcloud compute config-ssh --quiet
+
+gcloud compute ssh cultucat-back --zone=us-central1-a --tunnel-through-iap --project=cultucat-405114 << EOF
+  sudo service apache2 stop
+  cd backend/ || exit 1
+  source myenv/bin/activate
+  git pull
+  sudo service apache2 start
+EOF
